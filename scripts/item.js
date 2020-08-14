d0d202b69f31bdff6b04b3193322689725a31922
Jeffrey Chodil <jchodilpiano@gmail.com>
2020-08-14 10:12:12 -0400

    resolving conflicts

--- scripts/item.js
+++ scripts/item.js
@@ -1,5 +1,4 @@
 function validateName(name){
-<<<<<<< HEAD
     if (name === ''){
     throw new TypeError("Name must not be blank")};
 }
@@ -7,15 +6,6 @@ function validateName(name){
 function create(name){
     const newItem = {'id':cuid(), 'name':name, 'checked': false};
     return newItem;
-=======
-    if (name === "" ){
-    throw new TypeError("Name must not be blank")}
-}
-
-function create(name){
-    let newItem = {'id':cuid(), 'name':name, 'checked': false};
-    return newItem;    
->>>>>>> 40549bb8e76b295bdd66aca8b16f54422a85f5e0
 }
 
 export default {
